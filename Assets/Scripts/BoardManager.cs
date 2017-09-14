418b972726928f6eadec13c8c51b9ef61863b662
Eamon Leonard <Klagsy@users.noreply.github.com>
2017-09-13 18:00:28 -0700

    Add files via upload

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class BoardManager : MonoBehaviour {
+
+    
+
+    public int columns = 11;
+    public int rows = 8;
+    public GameObject[] grassTiles;
+    public GameObject[] wallTiles;
+    private Transform boardHolder;
+    
+    void boardSetup ()
+    {
+        boardHolder = new GameObject("board").transform;
+        for (int x=-1; x<columns+1; x++)
+        {
+            for (int y=-1; y<rows; y++)
+            {
+                GameObject toInstantiate = grassTiles[Random.Range (0, grassTiles.Length)];
+
+                if (x == -1 || x == columns|| x==5 || y == -1 || y == rows-1)
+                    toInstantiate = wallTiles[Random.Range (0, wallTiles.Length)];
+                GameObject instance =
+                    Instantiate(toInstantiate, new Vector3(x, y, 0f), Quaternion.identity) as GameObject;
+                instance.transform.SetParent(boardHolder);
+            }
+        }
+    }
+
+	// Use this for initialization
+	public void SetupScene ()
+    {
+        boardSetup();
+    }
+
+}
---
+++ Assets/Scripts/Gamemanager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gamemanager : MonoBehaviour {
+
+    public static Gamemanager instance = null;
+    private BoardManager boardScript;
+
+	// Use this for initialization
+	void Awake () {
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+	}
+	
+	// Update is called once per frame
+	void InitGame () {
+        boardScript.SetupScene();
+	}
+}
